6971c92d5188f6b6adcc9e5a1b8751769242252c
Devon Navon <devon.navon@gmail.com>
2020-09-05 16:16:24 -0700

    no links

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">
-        noshi-portfolio
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">noshi-portfolio</h1>
     </div>
   </div>
 </template>
@@ -47,16 +27,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
