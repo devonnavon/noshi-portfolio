b94c8bd9d3d335c6e6dd98e19f9b798ac3174ad0
Devon Navon <devon.navon@gmail.com>
2020-09-07 15:27:52 -0700

    Revert "markdown test"
    This reverts commit f9c7d90af90b5677c4051b65110d0f0b76dc4615.

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h2>{{ post.title }}</h2>
-    <!-- <div>{{post.body}}</div> -->
-    <nuxt-content :document="post.body" />
+    <div>{{post.body}}</div>
+    <!-- <nuxt-content :document="post" /> -->
   </div>
 </template>
 
@@ -12,7 +12,6 @@ export default {
     let post
     try {
       post = await $content('blog', params.slug).fetch()
-      console.log(post.body)
       // OR const article = await $content(`articles/${params.slug}`).fetch()
     } catch (e) {
       error({ message: 'Blog Post not found' })
