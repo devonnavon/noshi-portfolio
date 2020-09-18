8010178772ad7ea9c4644e30fc97771efe6fef16
lshirase <lshirase@umich.edu>
2020-09-17 18:47:52 -0700

    barely did anything just look at the code man

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
+    <div id="growing" class="w-full h-screen mx-auto flex pl-5 xl:container">
+      <Growing class="w-full h-auto self-center" />
+    </div>
     <div class="bg-pink text-green h-screen w-full">
-      <div id="growing" class="w-full h-screen mx-auto flex pl-5 xl:container">
-        <Growing class="w-full h-auto self-center" />
-      </div>
       <div id="whatwedo" class="text-2xl text-center">what we do</div>
     </div>
   </div>
