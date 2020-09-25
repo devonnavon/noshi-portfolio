ff583936cae46acccf3816c08534b99f91602449
Devon Navon <devon.navon@gmail.com>
2020-09-25 16:43:02 -0700

    scroll animation stops after first scroll

--- components/HomepageServices.vue
+++ components/HomepageServices.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="whatwedo" class="parallax parallax--about bg-pink">
+  <section id="whatwedo" class="bg-pink">
     <div class="relative flex font-display h-screen">
       <div
         class="text-2xl text-green text-opacity-50 absolute inset-x-0 top-0 pl-10 pt-5"
@@ -139,15 +139,34 @@ export default {
           opacity: 100,
           y: 0,
         })
+        .add('end')
 
       ScrollTrigger.create({
         animation: tl,
         trigger: '#whatwedo',
         start: 'top top',
         end: '+=4000',
-        scrub: 1,
         pin: true,
+        // pinSpacing: false,
+        scrub: 1,
+        // once: true,
         anticipatePin: 1,
+        onLeave: (self) => {
+          self.disable()
+          gsap.set('#etc-detail', {
+            y: 0,
+            opacity: 100,
+          })
+          gsap.set('#design', { x: 0 })
+          gsap.set('#develop', { x: 0 })
+          gsap.set('#etc', { x: 0 })
+        },
+        snap: {
+          snapTo: 'labels', // snap to the closest label in the timeline
+          duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+          delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+          ease: 'power1.inOut', // the ease of the snap animation ("power3" by default)
+        },
       })
     },
   },
