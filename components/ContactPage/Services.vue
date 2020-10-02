dcf3bc9757c9fc469715329edb15e3029c5ccba9
lshirase <lshirase@umich.edu>
2020-10-02 16:06:37 -0700

    contact page woo

---
+++ components/ContactPage/Services.vue
@@ -0,0 +1,59 @@
+<template>
+  <section
+    id="whatwedo"
+    class="bg-pink mx-6 h-screen flex flex-col justify-center"
+  >
+    <div
+      class="flex flex-col font-serif text-green text-left w-full md:py-0 py-16"
+    >
+      <div class="md:text-6xl text-4xl">Let's make your next</div>
+      <div id="switcharooo" class="md:text-huge text-5xl font-semibold"></div>
+      <div class="self-end font-bold md:text-5xl text-3xl pt-16">together.</div>
+    </div>
+    <div class="py-12"></div>
+    <div
+      class="flex flex-col font-serif text-green text-center md:pt-4 md:w-2/3 w-full mx-auto md:py-0 py-16"
+    >
+      <div class="md:text-3xl text-xl">Say hello</div>
+      <div class="md:text-huge text-4xl self-center">(hey@noshi.cx)</div>
+      <!-- <div class="self-end font-bold text-4xl">together.</div> -->
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.switchArooAnimate()
+  },
+  data() {
+    return {
+      clickable: false,
+      currentService: null,
+      words: [
+        'web app',
+        'website',
+        'digital strategy',
+        'data pipeline',
+        'ecommerce store',
+      ],
+    }
+  },
+  methods: {
+    switchArooAnimate() {
+      let masterTl = gsap.timeline({ repeat: -1 })
+      this.words.forEach((word) => {
+        let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1 })
+        tl.to('#switcharooo', { duration: 1, text: word })
+        masterTl.add(tl)
+      })
+    },
+  },
+}
+</script>
+
+<style>
+#switcharooo {
+  min-height: 144px;
+}
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <h1>contact yo us</h1>
+  <div id="#container" class="xl:container mx-auto bg-pink md:px-6">
+    <Services />
   </div>
 </template>
+
+<script>
+import Services from '~/components/ContactPage/Services'
+
+export default {
+  components: {
+    Services,
+  },
+}
+</script>
+
+<style></style>
