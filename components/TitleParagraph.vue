14e2045c29586867a7196b07d413be6cd72701f1
Devon Navon <devon.navon@gmail.com>
2020-10-05 12:12:03 -0700

    engagementtypes

---
+++ components/TitleParagraph.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="text-3xl">{{ title }}</div>
+    <div class="text-xl">
+      {{ paragraph }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    paragraph: String,
+  },
+}
+</script>
+
+<style></style>
