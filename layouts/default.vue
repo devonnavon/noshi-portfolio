c0158a23308d50d7fe4742ed4b1d5e24c40734bc
Devon Navon <devon.navon@gmail.com>
2020-10-08 13:46:03 -0700

    cleared out old code on default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,58 +17,6 @@ export default {
     NavBar,
   },
   computed: { ...mapState(['page']) },
-  // mounted() {
-  //   console.log()
-  //   if (this.page === 'index') this.animate()
-  //   else this.showNav()
-  // },
-  // watch: {
-  //   page(newVal, oldVal) {
-  //     console.log('hello')
-  //     if (newVal === this.animate) this.animate()
-  //     else this.showNav()
-  //   },
-  // },
-  methods: {
-    animate() {
-      gsap.registerPlugin(ScrollTrigger)
-      //set tweens
-      gsap.set('#nav', {
-        y: -600,
-        opacity: 0,
-      })
-      gsap.to('#body', {
-        yPercent: -100,
-        ease: 'slow',
-        scrollTrigger: {
-          trigger: '#container',
-          start: 'top top',
-          end: '+=250',
-          scrub: 1,
-        },
-      })
-      gsap.to('#nav', {
-        opacity: 100,
-        delay: 3.5,
-        stagger: 0.3,
-        duration: 1.5,
-        y: 0,
-        ease: 'elastic.out(0.9, 1)',
-        scrollTrigger: {
-          trigger: '#container',
-          start: 'top top',
-          end: '+=250',
-          scrub: 1,
-        },
-      })
-    },
-    showNav() {
-      gsap.set('#nav', {
-        y: 0,
-        opacity: 100,
-      })
-    },
-  },
 }
 </script>
 <style></style>
