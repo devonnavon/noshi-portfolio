7fd4e1eea6b948f7b8d5bda167229b2990973992
lshirase <lshirase@umich.edu>
2020-10-14 12:29:57 -0700

    fixed typewriter going off

--- components/Homepage/Summary.vue
+++ components/Homepage/Summary.vue
@@ -1,7 +1,7 @@
 <template>
   <section
     id="summary"
-    class="bg-pink relative font-display w-screen md:w-auto pt-0 md:pt-14 flex flex-col justify-center align-center"
+    class="bg-pink relative font-display w-full md:w-auto pt-0 md:pt-14 flex flex-col justify-center align-center"
   >
     <div
       id="phrases"
