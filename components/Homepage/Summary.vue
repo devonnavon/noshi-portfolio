f905a157feff4a426b76c85cacd5eea9bf457ffd
lshirase <lshirase@umich.edu>
2020-10-14 14:40:29 -0700

    more summary fixes across screen sizes

--- components/Homepage/Summary.vue
+++ components/Homepage/Summary.vue
@@ -5,7 +5,7 @@
   >
     <div
       id="phrases"
-      class="relative text-green md:text-huge text-3xl md:pl-16 px-3 md:px-6"
+      class="relative text-green sm:text-4xl sm:px-6 xl:text-huge text-3xl md:pl-16 px-3 md:px-6"
     >
       <div v-for="(phrase, i) in phrases" :key="i" :id="`phrase_${i}`"></div>
     </div>
