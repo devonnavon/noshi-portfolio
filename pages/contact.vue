d61694aaa9470b863ccda5ec217a1ecc7952465b
Devon Navon <devon.navon@gmail.com>
2020-10-16 18:11:37 -0700

    contact us form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="#container" class="xl:container mx-auto bg-pink md:px-6">
     <Contact />
+    <ContactForm />
   </div>
 </template>
 
 <script>
 import Contact from '~/components/ContactPage/Contact'
+import ContactForm from '~/components/ContactPage/ContactForm'
 
 export default {
   head() {
@@ -15,6 +17,7 @@ export default {
   },
   components: {
     Contact,
+    ContactForm,
   },
 }
 </script>
