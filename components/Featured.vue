53d7c61be16880130cbee23a6ce5d417bac3b44b
lshirase <lshirase@umich.edu>
2020-11-03 14:04:57 -0800

    featured thign

---
+++ components/Featured.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    id="featured"
+    class="border-solid border-t-2 border-b-2 border-l-0 border-r-0 border-green md:mt-48 mt-24"
+  >
+    <ul class="topics font-display text-green text-2xl">
+      <li>FEAUTRED ON APPLE KEYNOTE</li>
+      <li>FEAUTRED ON APPLE KEYNOTE</li>
+
+      <li>FEAUTRED ON APPLE KEYNOTE</li>
+
+      <li>FEAUTRED ON APPLE KEYNOTE</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import SomethingSwitcher from '~/components/SomethingSwitcher'
+
+export default {
+  components: {
+    SomethingSwitcher,
+  },
+  mounted() {
+    this.contactScroll()
+    this.gearRotate()
+  },
+  methods: {
+    contactScroll() {
+      let topics = document.querySelector('.topics').cloneNode(true)
+      document.getElementById('news').appendChild(topics)
+
+      gsap.to('.topics', 30, {
+        x: -topics.offsetWidth,
+        repeat: -1,
+        ease: Linear.easeNone,
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+#featured {
+  //   border: solid 2px gray;
+  white-space: nowrap;
+  overflow: hidden;
+}
+#featured ul {
+  display: inline-block;
+  padding: 0px;
+  margin: 0px;
+}
+#featured li {
+  display: inline;
+  margin-right: 30px;
+}
+</style>
