a1f55242d9ea6e8894656df5d07e0c808ab00c18
Devon Navon <devon.navon@gmail.com>
2020-11-03 10:48:19 -0800

    work page read more shows up (not conidtionally) work page also shows up with data loaded

--- components/WorkPage/CaseCard.vue
+++ components/WorkPage/CaseCard.vue
@@ -61,6 +61,7 @@
                 v-html="$md.render(work.overview)"
                 class="page__content markdown"
               />
+              <NuxtLink :to="`work/${work.slug}`">Read More</NuxtLink>
             </div>
             <div class="flex flex-col pb-2 md:pb-0">
               <div class="text-3xl md:pb-4 pb-2">Roles</div>
---
+++ pages/work/_case.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>{{ work.details }}</div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.work.title,
+    }
+  },
+  async asyncData({ $content, params, error }) {
+    let work
+    console.warn(params)
+    try {
+      work = await $content('work', params.case).fetch()
+      //   work = await $content(`work/${params.slug}`).fetch()
+    } catch (e) {
+      error({ message: e })
+    }
+
+    return {
+      work,
+    }
+  },
+}
+</script>
+
+<style></style>
