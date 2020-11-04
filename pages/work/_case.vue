6acdf7c59054e1be7ac3d56a7e2cf023ab50e2fe
lshirase <lshirase@umich.edu>
2020-11-03 16:18:08 -0800

    getting case going

--- pages/work/_case.vue
+++ pages/work/_case.vue
@@ -1,12 +1,94 @@
 <template>
-  <div
-    v-html="$md.render(work.details)"
-    class="page__content markdown pt-4 md:pt-6 md:pb-24"
-  />
+  <div class="xl:container mx-auto bg-pink md:px-6 pt-24">
+    <div
+      class="flex flex-col font-display text-green md:py-8 py-4 px-8 mx-auto"
+    >
+      <div v-if="work.client == 'NFL + Verizon'" class="pb-2 md:pb-0">
+        <Featured2 />
+      </div>
+
+      <div class="flex-row text-2xl opacity-50 pb-2 hidden md:flex">
+        {{ work.client }}
+      </div>
+      <div class="flex flex-row">
+        <div class="flex flex-col w-1/4 md:w-1/3 flex-shrink-0">
+          <div class="case-thumbnail">
+            <img :src="work.thumbnail" class="w-full h-auto" alt="" />
+          </div>
+        </div>
+        <div class="flex flex-col pl-4 w-2/3 md:w-auto">
+          <div
+            class="text-xl md:text-3xl xl:text-6xl leading-none w-3/4 md:w-2/3"
+          >
+            {{ work.title }}
+          </div>
+          <div class="text-lg opacity-50 pb-2 md:hidden">{{ work.client }}</div>
+          <div
+            class="text-base pt-2 md:w-full lg:w-3/4 hidden md:inline-block text-justify"
+          >
+            {{ work.description }}
+          </div>
+        </div>
+      </div>
+      <div v-if="work.client == 'NFL + Verizon'" class="py-2"><Featured /></div>
+
+      <div class="md:py-4 py-2"></div>
+      <div class="h-auto">
+        <div class="flex md:flex-row flex-col-reverse">
+          <div class="flex flex-col md:w-3/4 w-full pr-12">
+            <div class="text-3xl pb-4">Overview</div>
+            <div
+              v-html="$md.render(work.overview)"
+              class="page__content markdown"
+            />
+          </div>
+          <div class="flex flex-col pb-2 md:pb-0">
+            <div class="text-3xl md:pb-4 pb-2">Roles</div>
+            <div class="flex flex-row flex-wrap md:flex-col justify-center">
+              <div
+                v-for="(role, i) in work.roles"
+                :key="i"
+                class="px-2 py-2 md:px-0 md:py-0 md:pb-1"
+              >
+                {{ role }}
+              </div>
+            </div>
+          </div>
+          <div
+            class="text-base flex md:hidden justify-center text-center italic py-4"
+          >
+            {{ work.description }}
+          </div>
+        </div>
+        <div
+          class="flex flex-col md:flex-row mx-auto md:justify-center items-center w-full md:w-2/3 py-4"
+        >
+          <img
+            v-for="(image, i) in work.galleryImages"
+            :src="image"
+            :key="i"
+            class="w-2/3 px-4"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-html="$md.render(work.details)"
+      class="page__content markdown overflow-hidden px-8 mx-auto font-display text-base text-green"
+    />
+  </div>
 </template>
 
 <script>
+import Featured from '~/components/Featured.vue'
+import Featured2 from '~/components/Featured2.vue'
+
 export default {
+  components: {
+    Featured,
+    Featured2,
+  },
   head() {
     return {
       title: this.work.title,
@@ -29,4 +111,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+h1 {
+  font-size: 1.875rem;
+  padding-bottom: 1rem;
+}
+</style>
