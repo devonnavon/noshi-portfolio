c0ac6f93246f870de1dd5ba74339a1ed9f49cae3
Devon Navon <devon.navon@gmail.com>
2020-11-09 15:51:50 -0800

    icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,17 +14,17 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'noshi design consulting',//process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'noshi is a two person, los angeles based, design studio focused on developing digital products',//process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: 'static/icon.png' }],
     script: [
       //gsap
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js' },
