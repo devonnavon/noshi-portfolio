adfe0a0d1790c3e9d5d3390554f8154b8fa55146
Devon Navon <devon.navon@gmail.com>
2020-11-09 16:06:18 -0800

    cleeanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
         content: 'noshi is a two person, los angeles based, design studio focused on developing digital products',//process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: 'icon.png' }],
+    link: [{
+      rel: 'icon', type: 'image/png', href: '/icon.png' }],
     script: [
       //gsap
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js' },
@@ -42,26 +43,6 @@ export default {
       {
         src:
           'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js',
-      },
-
-      //scrollmagic - dont think we need anythign below
-      {
-        src:
-          'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js',
-      },
-      {
-        src:
-          'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js',
-      },
-      {
-        src:
-          'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js',
-      },
-
-      {
-        //might not be needed (debugging)
-        src:
-          'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js',
       },
     ],
   },
