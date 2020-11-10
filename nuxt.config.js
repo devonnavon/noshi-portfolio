363ce09feaa43d895ab58ae158593b1914604331
lshirase <lshirase@umich.edu>
2020-11-09 16:51:35 -0800

    changed favicon paths

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,18 +14,24 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'noshi design consulting',//process.env.npm_package_name || '',
+    title: 'noshi design consulting', //process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'noshi is a two person, los angeles based, design studio focused on developing digital products',//process.env.npm_package_description || '',
+        content:
+          'noshi is a two person, los angeles based, design studio focused on developing digital products', //process.env.npm_package_description || '',
       },
     ],
-    link: [{
-      rel: 'icon', type: 'image/png', href: '/icon.png' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/icon.png',
+      },
+    ],
     script: [
       //gsap
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js' },
@@ -48,14 +54,51 @@ export default {
   },
   pwa: {
     icon: {
-      source:'~/static/icon.png'
+      src: '/icon.png',
     },
+
     manifest: {
       name: 'noshi design studio',
       short_name: 'noshi design studio',
       lang: 'en',
-      description: 'noshi is a two person, los angeles based, design studio focused on developing digital products'
-    }
+      description:
+        'noshi is a two person, los angeles based, design studio focused on developing digital products',
+      icons: [
+        {
+          src: '/icon.png',
+        },
+        {
+          src: '/favicon/favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png',
+        },
+        {
+          src: '/favicon/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+        },
+        {
+          src: '/favicon/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+        },
+        {
+          src: '/favicon/mstile-150x150.png',
+          sizes: '150x150',
+          type: 'image/png',
+        },
+        {
+          src: '/favicon/android-chrome-192x192-.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/favicon/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
   },
   /*
    ** Global CSS
