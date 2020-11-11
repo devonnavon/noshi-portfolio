1f0c764f8e9a1059dc68468e54c7918c91f4bafc
Devon Navon <devon.navon@gmail.com>
2020-11-11 11:54:10 -0800

    test change

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
           <ScrollAnchor
             anchor="summary"
             class="self-center justify-self-center text-center sm:mt-auto -mt-24 font-display text-l text-green"
-            >take a look
+            >be cool
           </ScrollAnchor>
         </div>
       </div>
